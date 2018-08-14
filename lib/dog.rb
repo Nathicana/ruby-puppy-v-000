daad8306771dbbfb71524e533839166e4ac8d387
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-08-14 04:29:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,23 @@ class Dog
 
 @@all = []
 
+attr_accessor :name
+
+ def initialize(name)
+   @name = name
+   @@all << self
+ end
+ 
+ def self.all 
+ @@all.each do |name|
+ puts song.name 
+ end
+ end
+
+def self.clear_all
+  self.delete 
+end
+
 
 
 
